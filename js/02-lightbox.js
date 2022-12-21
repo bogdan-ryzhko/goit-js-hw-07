3be00ca274482d1df148c8697258476be9b36a5b
Bogdan Ryzhko <m3meizumini@gmail.com>
2022-12-21 13:39:07 +0200

    add task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+const getMurkupImages = arr => {
+	return arr.map(({ preview, original, description }) => {
+		return `<a class="gallery__item" href="${original}">
+						<img class="gallery__image" src="${preview}" alt="${description}" />
+				</a>`
+	}).join('');
+};
+
+galleryRef.insertAdjacentHTML('beforeend', getMurkupImages(galleryItems));
+
+const imageHandler = (event) => {
+	event.preventDefault();
+	const targetIMG = event.target;
+
+	if (targetIMG.nodeName !== 'IMG') {
+		return;
+	}
+
+	new SimpleLightbox('.gallery a');
+}
+
+galleryRef.addEventListener('click', imageHandler);
