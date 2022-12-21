79d6999190553704a28cc6afddf99b29efe34e75
Bogdan Ryzhko <m3meizumini@gmail.com>
2022-12-21 23:04:54 +0200

    add title on image

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,14 +8,18 @@ const galleryRef = document.querySelector('.gallery');
 const getMurkupImages = arr => {
 	return arr.map(({ preview, original, description }) => {
 		return `<a class="gallery__item" href="${original}">
-						<img class="gallery__image" src="${preview}" alt="${description}" />
+						<img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
 				</a>`
 	}).join('');
 };
 
 galleryRef.insertAdjacentHTML('beforeend', getMurkupImages(galleryItems));
 
-let lightbox = new SimpleLightbox('.gallery .gallery__item');
+const options = {
+	captionDelay: 250,
+}
+
+let lightbox = new SimpleLightbox('.gallery .gallery__item', options);
 
 const imageHandler = (event) => {
 	event.preventDefault();
