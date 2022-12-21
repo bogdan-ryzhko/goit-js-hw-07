9925179bd88b7c8504cf80d23880f9ae168ace40
Bogdan Ryzhko <m3meizumini@gmail.com>
2022-12-21 23:14:40 +0200

    add captionDelay on caption

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,14 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
 const galleryRef = document.querySelector('.gallery');
 
 const getMurkupImages = arr => {
 	return arr.map(({ preview, original, description }) => {
 		return `<a class="gallery__item" href="${original}">
-						<img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+						<img class="gallery__image"
+						src="${preview}"
+						alt="${description}"
+						title="${description}"
 				</a>`
 	}).join('');
 };
